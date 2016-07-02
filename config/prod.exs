8f9addb70a4a58b98b818c5b6a46c5794ecf0820
Tony Han <h.bing612@gmail.com>
2016-07-02 19:12:19 +0800

    do not check_origin in prod

--- config/prod.exs
+++ config/prod.exs
@@ -7,8 +7,9 @@ use Mix.Config
 # You should also configure the url host to something
 # meaningful, we use this information when generating URLs.
 config :chat, Chat.Endpoint,
-  http: [port: {:system, "PORT"}],
-  url: [host: "example.com"]
+  http: [port: System.get_env("PORT") || 8080],
+  url: [host: "example.com"],
+  check_origin: false
 
 # ## SSL Support
 #
