fe7ad35c6b4b62d9fad5cfbc0a3fc8252c071544
Tony Han <h.bing612@gmail.com>
2016-07-02 18:54:44 +0800

    change prod logger to debug and uncomment serve_endpoints

--- config/prod.exs
+++ config/prod.exs
@@ -25,14 +25,14 @@ config :chat, Chat.Endpoint,
 # disk for the key and cert.
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # ## Using releases
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
 #
-#     config :phoenix, :serve_endpoints, true
+config :phoenix, :serve_endpoints, true
 #
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
